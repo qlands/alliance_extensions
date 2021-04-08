dd5365653bf022b5938b432c19916a19a1ad45ba
qlands <cquiros@qlands.com>
2021-04-08 14:59:27 -0600

    Implement FTP storage

--- alliance_submission/alliance_submission/plugin.py
+++ alliance_submission/alliance_submission/plugin.py
@@ -1,42 +1,49 @@
 import formshare.plugins as plugins
-import formshare.plugins.utilities as u
-from .views import MyPublicView, MyPrivateView
-import sys
+import ftplib
 import os
+import shutil
+from PIL import Image
+import imghdr
 
 
-class alliance_submission(plugins.SingletonPlugin):
-    plugins.implements(plugins.IRoutes)
-    plugins.implements(plugins.IConfig)
-    plugins.implements(plugins.ITranslation)
+class AllianceSubmission(plugins.SingletonPlugin):
+    plugins.implements(plugins.IMediaSubmission)
 
-    def before_mapping(self, config):
-        # We don't add any routes before the host application
-        return []
+    def before_storing_media(
+            self, request, user, project, form, assistant, submission, xml_file, media_file
+    ):
+        if project == request.registry.settings.get("ftp.project"):
+            image = imghdr.what(media_file)
+            if image is None:
+                image = False
+            else:
+                image = True
+            if image:
+                ftp_server = request.registry.settings.get("ftp.server")
+                ftp_user = request.registry.settings.get("ftp.user")
+                ftp_password = request.registry.settings.get("ftp.password")
+                ftp_path = request.registry.settings.get("ftp.path")
+                try:
+                    session = ftplib.FTP(ftp_server, ftp_user, ftp_password)
+                    file_name = submission + "_" + os.path.basename(media_file)
+                    file = open(media_file, 'rb')
+                    ftp_file = os.path.join(ftp_path, *[file_name])
+                    session.storbinary('STOR {}'.format(ftp_file), file)
+                    file.close()  # close file and FTP
+                    session.quit()
+                except Exception as e:
+                    print("FTPStorageError: Cannot store file {} in FTP server. Error: {}".format(media_file, str(e)))
+                    ftp_fallback_path = request.registry.settings.get("ftp.fallback.path")
+                    file_name = submission + "_" + os.path.basename(media_file)
+                    fallback_file = os.path.join(ftp_fallback_path, *[file_name])
+                    shutil.copy(media_file, fallback_file)
+                # Reducing media file to 100X100 thumbnail
+                im = Image.open(media_file)
+                im.thumbnail((100, 100))
+                im.save(media_file)
+        return True
 
-    def after_mapping(self, config):
-        # We add here a new route /json that returns a JSON
-        custom_map = [
-            u.add_route(
-                "plugin_mypublicview", "/mypublicview", MyPublicView, "public.jinja2"
-            ),
-            u.add_route(
-                "plugin_myprivateview",
-                "/user/{userid}/myprivateview",
-                MyPrivateView,
-                "private.jinja2",
-            ),
-        ]
-
-        return custom_map
-
-    def update_config(self, config):
-        # We add here the templates of the plugin to the config
-        u.add_templates_directory(config, "templates")
-
-    def get_translation_directory(self):
-        module = sys.modules["alliance_submission"]
-        return os.path.join(os.path.dirname(module.__file__), "locale")
-
-    def get_translation_domain(self):
-        return "alliance_submission"
+    def after_storing_media(
+            self, request, user, project, form, assistant, submission, xml_file, media_file
+    ):
+        pass
--- alliance_submission/setup.py
+++ alliance_submission/setup.py
@@ -32,5 +32,5 @@ setup(
     zip_safe=False,
     extras_require={"testing": tests_require},
     install_requires=requires,
-    entry_points={"formshare.plugins": ["alliance_submission = alliance_submission.plugin:alliance_submission"]},
+    entry_points={"formshare.plugins": ["alliance_submission = alliance_submission.plugin:AllianceSubmission"]},
 )
