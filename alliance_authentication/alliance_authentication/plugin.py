5cb1ed6e1550dcfe6eef3bb2bc9d74fafb3ee61c
qlands <cquiros@qlands.com>
2021-04-15 11:15:24 -0600

    Avoid waiting for CGIAR AD

--- alliance_authentication/alliance_authentication/plugin.py
+++ alliance_authentication/alliance_authentication/plugin.py
@@ -4,6 +4,7 @@ import sys
 import os
 from subprocess import Popen, PIPE
 import logging
+from threading import Timer
 
 log = logging.getLogger("formshare")
 
@@ -34,6 +35,7 @@ class alliance_authentication(plugins.SingletonPlugin):
         return None, {}
 
     def on_authenticate_password(self, request, user_data, password):
+        _ = request.translate
         java_java = request.registry.settings.get("java.authentication.jar")
         args = ["java", "-jar", java_java]
         if (
@@ -45,9 +47,21 @@ class alliance_authentication(plugins.SingletonPlugin):
         args.append(user_data["user_email"])
         args.append("-p")
         args.append(password)
+        time_out_flag = False
+
+        def kill_time_out(process):
+            nonlocal time_out_flag
+            time_out_flag = True
+            process.kill()
+
         try:
             p = Popen(args, stdout=PIPE, stderr=PIPE)
-            stdout, stderr = p.communicate()
+            time_out = Timer(5, kill_time_out, [p])  # Wait 5 seconds to authenticate with CGIAR AD
+            try:
+                time_out.start()
+                stdout, stderr = p.communicate()
+            finally:
+                time_out.cancel()
         except Exception as e:
             log.error(
                 "CGIARADAuthenticationError: "
@@ -55,10 +69,12 @@ class alliance_authentication(plugins.SingletonPlugin):
                     user_data["user_email"], str(e)
                 )
             )
-            return False
+            return False, _("Error while authenticating your account with the CGIAR AD")
+        if time_out_flag:
+            return False, _("Timeout while authenticating your account with the CGIAR AD")
         error_no = p.returncode
         if error_no == 0:
-            return True
+            return True, ""
         else:
             if error_no == 1:
                 log.error(
@@ -67,6 +83,7 @@ class alliance_authentication(plugins.SingletonPlugin):
                         user_data["user_email"], stdout.decode(), stderr.decode()
                     )
                 )
+                return False, _("Error while authenticating your account with the CGIAR AD")
             if error_no == 2:
                 log.error(
                     "CGIARADAuthenticationError: "
@@ -74,7 +91,7 @@ class alliance_authentication(plugins.SingletonPlugin):
                         user_data["user_email"]
                     )
                 )
-            return False
+                return False, _("The email account does not exist or the password is invalid")
 
     def after_collaborator_login(self, request, collaborator):
         return True, ""
