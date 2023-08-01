5a966af865b95ade99213274ab9f66af3629ed00
qlands <cquiros@qlands.com>
2023-07-31 21:18:23 -0400

    Allow external emails

--- alliance_authentication/alliance_authentication/plugin.py
+++ alliance_authentication/alliance_authentication/plugin.py
@@ -1,5 +1,6 @@
 import formshare.plugins as plugins
 import formshare.plugins.utilities as u
+from formshare.config.auth import check_login
 import sys
 import os
 from subprocess import Popen, PIPE
@@ -36,82 +37,85 @@ class alliance_authentication(plugins.SingletonPlugin):
 
     def on_authenticate_password(self, request, user_data, password):
         _ = request.translate
-        java_java = request.registry.settings.get("java.authentication.jar")
-        args = ["java", "-jar", java_java]
-        if (
-            request.registry.settings.get("java.authentication.remote", "false")
-            == "true"
-        ):
-            args.append("-r")
-        args.append("-e")
-        args.append(user_data["user_email"])
-        args.append("-p")
-        args.append(password)
-        time_out_flag = False
+        if user_data["user_email"].strip().lower().find("@cgiar.org") >= 0:
+            java_java = request.registry.settings.get("java.authentication.jar")
+            args = ["java", "-jar", java_java]
+            if (
+                request.registry.settings.get("java.authentication.remote", "false")
+                == "true"
+            ):
+                args.append("-r")
+            args.append("-e")
+            args.append(user_data["user_email"])
+            args.append("-p")
+            args.append(password)
+            time_out_flag = False
 
-        def kill_time_out(process):
-            nonlocal time_out_flag
-            time_out_flag = True
-            process.kill()
+            def kill_time_out(process):
+                nonlocal time_out_flag
+                time_out_flag = True
+                process.kill()
 
-        try:
-            p = Popen(args, stdout=PIPE, stderr=PIPE)
-            time_out = Timer(
-                30, kill_time_out, [p]
-            )  # Wait 5 seconds to authenticate with CGIAR AD
             try:
-                time_out.start()
-                stdout, stderr = p.communicate()
-            finally:
-                time_out.cancel()
-        except Exception as e:
-            if not time_out_flag:
-                log.error(
-                    "CGIARADAuthenticationError: "
-                    "Error while excuting Java authenticating user {}. Error: {}".format(
-                        user_data["user_email"], str(e)
+                p = Popen(args, stdout=PIPE, stderr=PIPE)
+                time_out = Timer(
+                    30, kill_time_out, [p]
+                )  # Wait 5 seconds to authenticate with CGIAR AD
+                try:
+                    time_out.start()
+                    stdout, stderr = p.communicate()
+                finally:
+                    time_out.cancel()
+            except Exception as e:
+                if not time_out_flag:
+                    log.error(
+                        "CGIARADAuthenticationError: "
+                        "Error while excuting Java authenticating user {}. Error: {}".format(
+                            user_data["user_email"], str(e)
+                        )
                     )
-                )
-                return (
-                    False,
-                    _("Error while authenticating your account with the CGIAR AD"),
-                )
-            else:
+                    return (
+                        False,
+                        _("Error while authenticating your account with the CGIAR AD"),
+                    )
+                else:
+                    return (
+                        False,
+                        _("Timeout while authenticating your account with the CGIAR AD"),
+                    )
+            if time_out_flag:
                 return (
                     False,
                     _("Timeout while authenticating your account with the CGIAR AD"),
                 )
-        if time_out_flag:
-            return (
-                False,
-                _("Timeout while authenticating your account with the CGIAR AD"),
-            )
-        error_no = p.returncode
-        if error_no == 0:
-            return True, ""
+            error_no = p.returncode
+            if error_no == 0:
+                return True, ""
+            else:
+                if error_no == 1:
+                    log.error(
+                        "CGIARADAuthenticationError: "
+                        "Error while authenticating user {}. Error: {}-{}".format(
+                            user_data["user_email"], stdout.decode(), stderr.decode()
+                        )
+                    )
+                    return (
+                        False,
+                        _("Error while authenticating your account with the CGIAR AD"),
+                    )
+                if error_no == 2:
+                    log.error(
+                        "CGIARADAuthenticationError: "
+                        "User {} provided an invalid password".format(
+                            user_data["user_email"]
+                        )
+                    )
+                    return (
+                        False,
+                        _("The email account does not exist or the password is invalid"),
+                    )
         else:
-            if error_no == 1:
-                log.error(
-                    "CGIARADAuthenticationError: "
-                    "Error while authenticating user {}. Error: {}-{}".format(
-                        user_data["user_email"], stdout.decode(), stderr.decode()
-                    )
-                )
-                return (
-                    False,
-                    _("Error while authenticating your account with the CGIAR AD"),
-                )
-            if error_no == 2:
-                log.error(
-                    "CGIARADAuthenticationError: "
-                    "User {} provided an invalid password".format(
-                        user_data["user_email"]
-                    )
-                )
-                return (
-                    False,
-                    _("The email account does not exist or the password is invalid"),
-                )
+            return check_login(user_data["user_id"], password, request)
 
     def after_collaborator_login(self, request, collaborator):
         return True, ""
